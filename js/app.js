d0d5ec7d66f1438c2ba4d7295aed8e69d2e89e7a
Dministry <dministry356@gmail.com>
2020-11-11 17:04:02 +0100

    updated

---
+++ js/app.js
@@ -0,0 +1,30 @@
+document.querySelector(".contact-form").addEventListener("submit", submitForm);
+
+function submitForm(e){
+    e.preventDefault();
+    let name = document.querySelector("#name").value
+    let email = document.querySelector("#email").value
+    let message = document.querySelector("#message").value
+    document.querySelector(".contact-form").reset();
+    sendEmail(name, email, message);
+}
+
+function sendEmail(name, email, message) { 
+    Email.send({ 
+      Host: "smtp.gmail.com", 
+      Username: "dministry356@gmail.com", 
+      Password: "sjinzzhhhloumuig", 
+      To: "dministry356@gmail.com",
+      From: "dministry356@gmail.com", 
+      Subject: `${name}: Sending ${email} using javascript`, 
+      Body: `Well that was easy!! ${message}`, 
+    //   Attachments: [ 
+    //     { 
+    //       name: "File_Name_with_Extension", 
+    //       path: "Full Path of the file" 
+    //     }] 
+    }).then(function (message) { 
+        alert("Mail has been sent successfully");
+      }); 
+  } 
+  sendEmail(name, email, message);
